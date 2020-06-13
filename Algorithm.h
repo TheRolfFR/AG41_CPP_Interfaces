f138aaa84ba1d1cc7afa64b42d9ec0b442bc19fb
TheRolf <yann.lv@live.fr>
2020-06-13 10:02:30 +0200

    HeuresJournée et HeuresSemaine

--- Algorithm.h
+++ Algorithm.h
@@ -1,16 +1,21 @@
 
 #ifndef AG41_PROJET_ALGORITHM_H
 #define AG41_PROJET_ALGORITHM_H
+
+#include <map>
 #include "Matrice.h"
 #include "jeuEssai.h"
 
-#define INDEX_DEBUT_FORMATION 4
-#define INDEX_FIN_FORMATION 5
+#define INDICE_JOUR_FORMATION 3
+#define INDICE_DEBUT_FORMATION 4
+#define INDICE_FIN_FORMATION 5
+
+#define JOURS_DANS_LA_SEMAINE 7
 
 class Algorithm 
 {
 private : 
-
+    std::map<int, int> _choix;
 public : 
     void solve_algorithm(Matrice M, int iteration, double eval_parent)
     {
@@ -32,10 +37,42 @@ public :
     void create_matrice();
     void eval();
 
-    long dureeFormation(int ligneFormation) {
-        return formation[ligneFormation][INDEX_FIN_FORMATION] - formation[ligneFormation][INDEX_FIN_FORMATION];
+    long dureeFormation(int indiceFomation) {
+        return formation[indiceFomation][INDICE_FIN_FORMATION] - formation[indiceFomation][INDICE_DEBUT_FORMATION];
     }
 
+    long heuresDuJour(int interface, int jour) {
+        long somme = 0;
+
+        // pour chaque choix dans _choix fait
+        int indiceFormation;
+        int interfaceAffectee;
+        int jourDeLaTache;
+        for(std::map<int, int>::iterator c = _choix.begin(); c != _choix.end(); c++) {
+            indiceFormation = c->first;
+            interfaceAffectee = c->second;
+
+            jourDeLaTache = formation[indiceFormation][INDICE_JOUR_FORMATION];
+
+            // si la tache est affectee l'interface choisie correspond (valeur) et que le jour correspond
+            if(interfaceAffectee == interface && jourDeLaTache == jour) {
+                // alors on ajoute la duree de l formation
+                somme += dureeFormation(indiceFormation);
+            }
+        }
+
+        return somme;
+    }
+
+    long heuresDeLaSemaine(int interface) {
+        long somme;
+
+        for(int i = 0; i < JOURS_DANS_LA_SEMAINE; ++i) {
+            somme += heuresDuJour(interface, i+1);
+        }
+
+        return somme;
+    }
 };
 
 #endif //AG41_PROJET_ALGORITHM
